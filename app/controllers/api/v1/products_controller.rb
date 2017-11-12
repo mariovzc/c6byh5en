eb8d607bde362c3bec40080f350894109759d572
¨Mario <mario.vizcainos@gmail.com>
2017-11-11 22:32:22 -0500

    update of status code

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::ProductsController < ApplicationController
   def create
     @product = Product.new(name: params[:name], price: params[:price])
     if  @product.save
-     render json: @product, status: 202
+     render json: @product, status: 201
     else
       render json: @product.errors, status: 422
     end
