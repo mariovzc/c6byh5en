40178a9dd13791ee975665f67458cfc276d53d62
¨Mario <mario.vizcainos@gmail.com>
2017-11-11 22:47:14 -0500

    setup product update

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::ProductsController < ApplicationController
-
+  before_filter :set_product, only: [:update, :destroy]
   def index
     @products = Product.all
   end
   def create
-    @product = Product.new(name: params[:name], price: params[:price])
+    @product = Product.new(produt_params)
     if  @product.save
      render json: @product, status: 201
     else
@@ -12,7 +12,19 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
   def update
+    if  @product.update(produt_params)
+      render json: @product, status: 200      
+    else
+      render json: @product.errors, status: 422
+    end
   end
   def destroy
+  end
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
+  def produt_params
+    params.permit(:name, :price)    
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :price, presence: true, numericality: true
+  
 end
