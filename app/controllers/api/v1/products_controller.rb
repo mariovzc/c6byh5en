004991fa7f953f26a8d216aee81e65d9654eda42
¨Mario <mario.vizcainos@gmail.com>
2017-11-11 22:30:53 -0500

    create products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,5 +3,12 @@ class Api::V1::ProductsController < ApplicationController
   def index
     @products = Product.all
   end
-  
+  def create
+    @product = Product.new(name: params[:name], price: params[:price])
+    if  @product.save
+     render json: @product, status: 202
+    else
+      render json: @product.errors, status: 422
+    end
+  end
 end
